560e50f1783c228fb7d9eb675886c64d04a8f0ba
Xein <xgonip@gmail.com>
2025-11-03 09:15:33 +0100

    idk whats this

--- lua/plugins/yazi.lua
+++ lua/plugins/yazi.lua
@@ -6,7 +6,7 @@ return {
 		{ "nvim-lua/plenary.nvim", lazy = true },
 	},
 	keys = {
-		{ "<leader>e", mode = { "n", "v" }, "<cmd>Yazi<cr>", desc = "Open yazi at the current file", },
+		{ "<leader>e", mode = { "n", "v" }, "<cmd>Ex<cr>", desc = "Open yazi at the current file", },
 		{ "<leader>E", "<cmd>Yazi cwd<cr>", desc = "Open the file manager in nvim's working directory", },
 		{ "<c-up>", "<cmd>Yazi toggle<cr>", desc = "Resume the last yazi session", },
 	},
